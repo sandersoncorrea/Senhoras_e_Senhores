8e8c67c72e26704bdec2f176110444b1aa7c4a1a
Sanderson Silva Corrêa <correasanderson@gmail.com>
2020-10-17 15:38:28 -0300

    Finalizado o front e final da aula 3

--- src/pages/CreateAsilo.tsx
+++ src/pages/CreateAsilo.tsx
@@ -1,56 +1,137 @@
-import React from "react";
+import React, { useState, FormEvent, ChangeEvent } from "react";
 import { Map, Marker, TileLayer } from "react-leaflet";
+import { LeafletMouseEvent } from "leaflet";
 import { FiPlus } from "react-icons/fi";
 
 import Sidebar from "../components/Sidebar";
 import "../styles/pages/create-asilo.css";
 import mapIcon from "../utils/mapIcon";
+import api from "../services/api";
+import { useHistory } from "react-router-dom";
 
 export default function CreateAsilo() {
+  const history = useHistory();
+  const [position, setPositiion] = useState({ latitude: 0, longitude: 0 });
+
+  const [name, setName] = useState("");
+  const [about, setAbout] = useState("");
+  const [instructions, setInstructions] = useState("");
+  const [opening_hours, setOpeningHours] = useState("");
+  const [open_on_weeks, setOpenOnWeeks] = useState(true);
+  const [images, setImages] = useState<File[]>([]);
+  const [previewImages, setPreviewImages] = useState<string[]>([]);
+
+  function handleMapClick(event: LeafletMouseEvent) {
+    const { lat, lng } = event.latlng;
+    setPositiion({
+      latitude: lat,
+      longitude: lng,
+    });
+  }
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    const { latitude, longitude } = position;
+
+    const data = new FormData();
+
+    data.append("name", name);
+    data.append("about", about);
+    data.append("latitude", String(latitude));
+    data.append("longitude", String(longitude));
+    data.append("instructions", instructions);
+    data.append("opening_hours", opening_hours);
+    data.append("open_on_weeks", String(open_on_weeks));
+    images.forEach((img) => {
+      data.append("images", img);
+    });
+
+    await api.post("/asilos", data);
+
+    alert("Cadastro realizado");
+    history.push("/map");
+  }
+
+  function handleSelectImages(event: ChangeEvent<HTMLInputElement>) {
+    const { files } = event.target;
+    if (!files) return;
+    const selectedImages = Array.from(files);
+    setImages([...selectedImages, ...images]);
+
+    const selectedImagesPreviews = selectedImages.map((image) => {
+      return URL.createObjectURL(image);
+    });
+
+    setPreviewImages([...selectedImagesPreviews, ...previewImages]);
+  }
+
   return (
     <div id="page-create-asilo">
       <Sidebar />
       <main>
-        <form className="create-asilo-form">
+        <form onSubmit={handleSubmit} className="create-asilo-form">
           <fieldset>
             <legend>Dados</legend>
 
             <Map
-              center={[-27.2092052, -49.6401092]}
+              center={[-21.7482345, -41.3330914]}
               style={{ width: "100%", height: 280 }}
               zoom={15}
+              onclick={handleMapClick}
             >
               <TileLayer
-                url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                url={`https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
               />
-
-              <Marker
-                interactive={false}
-                icon={mapIcon}
-                position={[-27.2092052, -49.6401092]}
-              />
+              {position.latitude !== 0 && (
+                <Marker
+                  interactive={false}
+                  icon={mapIcon}
+                  position={[position.latitude, position.longitude]}
+                />
+              )}
             </Map>
 
             <div className="input-block">
               <label htmlFor="name">Nome</label>
-              <input id="name" />
+              <input
+                id="name"
+                value={name}
+                onChange={(event) => setName(event.target.value)}
+              />
             </div>
 
             <div className="input-block">
               <label htmlFor="about">
                 Sobre <span>Máximo de 300 caracteres</span>
               </label>
-              <textarea id="name" maxLength={300} />
+              <textarea
+                id="name"
+                maxLength={300}
+                value={about}
+                onChange={(event) => setAbout(event.target.value)}
+              />
             </div>
 
             <div className="input-block">
               <label htmlFor="images">Fotos</label>
 
-              <div className="uploaded-image"></div>
-
-              <button className="new-image">
-                <FiPlus size={24} color="#15b6d6" />
-              </button>
+              <div className="images-container">
+                {previewImages.map((img) => {
+                  return <img key={img} src={img} alt="IMAGEM" />;
+                })}
+                {/* <button type="button" className="new-image">
+                  <FiPlus size={24} color="#15b6d6" />
+                </button> */}
+                <label htmlFor="image[]" className="new-image">
+                  <FiPlus size={24} color="#15b6d6" />
+                </label>
+              </div>
+              <input
+                multiple
+                onChange={handleSelectImages}
+                type="file"
+                id="image[]"
+              />
             </div>
           </fieldset>
 
@@ -59,22 +140,40 @@ export default function CreateAsilo() {
 
             <div className="input-block">
               <label htmlFor="instructions">Instruções</label>
-              <textarea id="instructions" />
+              <textarea
+                id="instructions"
+                value={instructions}
+                onChange={(event) => setInstructions(event.target.value)}
+              />
             </div>
 
             <div className="input-block">
-              <label htmlFor="opening_hours">Nome</label>
-              <input id="opening_hours" />
+              <label htmlFor="opening_hours">Horário de Funcionamento</label>
+              <input
+                id="opening_hours"
+                value={opening_hours}
+                onChange={(event) => setOpeningHours(event.target.value)}
+              />
             </div>
 
             <div className="input-block">
               <label htmlFor="open_on_weekends">Atende fim de semana</label>
 
               <div className="button-select">
-                <button type="button" className="active">
+                <button
+                  onClick={() => setOpenOnWeeks(true)}
+                  type="button"
+                  className={open_on_weeks ? "active" : ""}
+                >
                   Sim
                 </button>
-                <button type="button">Não</button>
+                <button
+                  onClick={() => setOpenOnWeeks(false)}
+                  type="button"
+                  className={!open_on_weeks ? "active" : ""}
+                >
+                  Não
+                </button>
               </div>
             </div>
           </fieldset>
