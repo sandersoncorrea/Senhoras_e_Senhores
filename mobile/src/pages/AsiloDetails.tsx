39e0261531f2774f89f635b873ab7f4b7b3247de
Sanderson Silva Corrêa <correasanderson@gmail.com>
2020-10-18 12:08:23 -0300

    mobile paginas de Map e Details do asilo

---
+++ mobile/src/pages/AsiloDetails.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { View, Text} from 'react-native';
+
+export default function AsiloDetails(){
+    return (<View><Text>Details</Text></View>);
+}
---
+++ mobile/src/routes.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { NavigationContainer} from '@react-navigation/native';
+import { createStackNavigator} from '@react-navigation/stack';
+
+const { Navigator, Screen } = createStackNavigator();
+
+import Map from './pages/Map';
+import AsiloDetails from './pages/AsiloDetails';
+
+export default function Routes() {
+    return (
+        <NavigationContainer>
+            <Navigator screenOptions={{ headerShown: false}}>
+                <Screen name="Map" component={Map} />
+                <Screen name="AsiloDetails" component={AsiloDetails} />
+            </Navigator>
+        </NavigationContainer>
+    )
+}
